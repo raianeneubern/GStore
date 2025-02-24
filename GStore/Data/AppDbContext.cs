ad725514fbc4661f5a3b7a18122bd9ebbcec70cc
raianeneubern <raianeneubern@gmail.com>
2025-02-24 08:08:23 -0300

    Finalizada a criação com o banco

--- GStore/Data/AppDbContext.cs
+++ GStore/Data/AppDbContext.cs
@@ -1,6 +1,35 @@
+using GStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace GStore.Data;
 
-public class AppDbContext
+public class AppDbContext : IdentityDbContext<Usuario>
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+    {
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<ProdutoFoto> ProdutoFotos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        #region Renomear Tabelas do Identity
+        builder.Entity<IdentityUser>().ToTable("usuario");
+        builder.Entity<IdentityUserRole<string>>().ToTable("usuario_perfil");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("usuario_login");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("usuario_regra");
+        builder.Entity<IdentityUserToken<string>>().ToTable("usuario_token");
+        builder.Entity<IdentityRole>().ToTable("perfil");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("perfil_regra");
+        #endregion
+
         
+
+    }
 }
--- GStore/Program.cs
+++ GStore/Program.cs
@@ -1,9 +1,33 @@
+using GStore.Data;
+using GStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+// Configuração do Serviço de Conexão com o banco de dados
+string conexao = builder.Configuration.GetConnectionString("GStoreConn");
+builder.Services.AddDbContext<AppDbContext>(
+    opt => opt.UseMySQL(conexao)
+);
+
+// Configuração do Serviço de Identity de Usuários
+builder.Services.AddIdentity<Usuario, IdentityRole>(
+    options => options.SignIn.RequireConfirmedEmail = false
+).AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope()) {
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await  dbContext.Database.EnsureCreatedAsync();
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
