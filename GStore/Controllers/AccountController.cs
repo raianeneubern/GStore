95472e122549320844da89b4a112e17782213f1c
raianeneubern <raianeneubern@gmail.com>
2025-04-11 10:54:50 -0300

    10/65

---
+++ GStore/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+using GStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace GStore.Controllers;
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<Usuario> _signInManager;
+    private readonly UserManager<Usuario> _userManager;
+    private readonly IWebHostEnvironment _host;
+
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<Usuario> signInManager,
+        UserManager<Usuario> userManager,
+        IWebHostEnvironment host
+    )
+    {
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _host = host;
+    }
+}
